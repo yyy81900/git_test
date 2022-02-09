a8e115954d631c922eee86f37ca15775e8335e88
Yyy <y81900@foxmail.com>
2022-02-09 20:38:28 +0800

    first commit

---
+++ src/main/java/com/yyy/git/GitTest.java
@@ -0,0 +1,41 @@
+package com.yyy.git;
+
+/**
+ * @author yyy
+ */
+public class GitTest {
+    private String name;
+    private Integer id;
+
+    public GitTest() {
+    }
+
+    public GitTest(String name, Integer id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "GitTest{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
