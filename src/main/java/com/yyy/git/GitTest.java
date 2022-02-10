86b64c34ea3c891a348fc3d6a7d5ef26065ad4dc
Yyy <y81900@foxmail.com>
2022-02-11 02:56:16 +0800

    hot-fix commit2

--- src/main/java/com/yyy/git/GitTest.java
+++ src/main/java/com/yyy/git/GitTest.java
@@ -42,6 +42,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world! 1");
         System.out.println("hello world! 2");
+        System.out.println("hello world! hot-fix");
+
 
     }
 }
