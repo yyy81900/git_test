099f24728f8a909c17b078911fe848c417fc3930
Yyy <y81900@foxmail.com>
2022-02-11 02:23:23 +0800

    second commit

--- src/main/java/com/yyy/git/GitTest.java
+++ src/main/java/com/yyy/git/GitTest.java
@@ -38,4 +38,8 @@ public class GitTest {
                 ", id=" + id +
                 '}';
     }
+
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
 }
